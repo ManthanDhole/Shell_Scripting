f76a479482542fb3ba8af6763484587e8db57b6a
Manthan Dhole <85555944+ManthanDhole@users.noreply.github.com>
2023-09-16 17:01:01 +0530

    Show which shells are present in the OS

--- hello.sh
+++ hello.sh
@@ -1,5 +1,9 @@
 # '#!' is called as HashBang / shBang which denotes the location of the Bash script executor in the OS 
 #! /usr/bin/bash
+
+# Shows which are the available shells in the OS
+echo cat /etc/shells
+
 echo "Hello World"
 echo $BASH  #Upper case for System Variables
 echo $BASH_VERSION
